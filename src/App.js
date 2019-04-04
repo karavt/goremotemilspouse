ab0c8790addff9645639f345633928e105400368
Kara <31089896+karavt@users.noreply.github.com>
2019-04-04 15:57:15 +0900

    get ready for build

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import MainForm from "./components/MainForm";
 import { Container } from "semantic-ui-react";
+import { browserHistory, Router, Route } from 'react-router';
+
 
 class App extends Component {
   render() {
     return (
       <Container textAlign="center">
         <MainForm />
+        <Route exact path="/quiz/" component={MainForm} />
       </Container>
     );
   }
